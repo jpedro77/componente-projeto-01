6b1c50417eb82db3688181efc7af3c53abd55935
João Pedro <jpedrofernandes77@gmail.com>
2021-04-12 23:48:50 -0300

    desafio curso vue - COD3R

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <navigation />
+
+    <bar />
+
+    <carrosel />
+
+    <footer-app />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from '@/components/Navigation'
+import Bar from '@/components/Bar'
+import Carrosel from '@/components/Carrosel'
+import Footer from '@/components/Footer'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    navigation: Navigation,
+    bar: Bar,
+    carrosel: Carrosel,
+    footerApp: Footer
+  },
+  data() {
+    return {
+
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
